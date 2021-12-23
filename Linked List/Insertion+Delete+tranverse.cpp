4b2d22df413ff780dfac8b0aa1b5a893f9703591
Vishal Karhade <68017086+vk2611@users.noreply.github.com>
2021-12-23 19:19:28 +0530

    Update Insertion+Delete+tranverse.cpp

--- Linked List/Insertion+Delete+tranverse.cpp
+++ Linked List/Insertion+Delete+tranverse.cpp
@@ -1,1 +1,33 @@
 
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+struct Node{
+	int data;
+	Node *next;
+};
+int main(){
+	int n;cin>>n;
+	Node *head=NULL;
+	Node *tail=NULL;
+	for(int i=0;i<n;i++){
+		ll val;cin>>val;
+		Node *t=new Node();
+		t->data=val;
+		if(head==NULL){
+			head=t;
+			tail=t;
+		}
+		else{
+			tail->next=t;
+			tail=tail->next;
+		}
+	}
+	while(head!=NULL){
+		cout<<head->data<<" ";
+		head=head->next;
+	}
+	
+}
